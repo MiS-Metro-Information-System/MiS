2f7dd206b8983fa0e4c34d2ac8138e8d1365923f
felipe.gutierrez <felipegi91@gmail.com>
2015-07-06 08:40:21 -0500

    created DbHelper created Sqlite Manager with Crud operations

---
+++ app/src/main/java/iomis/github/mis_metro_information_system/mis/FragmentHome.java
@@ -0,0 +1,15 @@
+package iomis.github.mis_metro_information_system.mis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by felipe.gutierrez on 03/07/2015.
+ */
+public class FragmentHome extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+    }
+}
