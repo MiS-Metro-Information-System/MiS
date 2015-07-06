b53705da58d3362348f86bbc9e24d57c69d70e21
felipe.gutierrez <felipegi91@gmail.com>
2015-07-06 10:20:16 -0500

    fragments for each view

--- app/src/main/java/iomis/github/mis_metro_information_system/mis/MainActivity.java
+++ app/src/main/java/iomis/github/mis_metro_information_system/mis/MainActivity.java
@@ -134,7 +134,7 @@ public class MainActivity  extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_wallet, container, false);
             return rootView;
         }
 
