257f3954fd8e0941cdbaa7e1bf82564920e92bb2
felipe.gutierrez <felipegi91@gmail.com>
2015-07-06 13:29:09 -0500

    google maps added full screen working on markets and routes

--- app/src/main/java/iomis/github/mis_metro_information_system/mis/FragmentRoutes.java
+++ app/src/main/java/iomis/github/mis_metro_information_system/mis/FragmentRoutes.java
@@ -1,5 +1,6 @@
 package iomis.github.mis_metro_information_system.mis;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,12 +12,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 /**
  * Created by felipe.gutierrez on 06/07/2015.
  */
 public class FragmentRoutes extends Fragment {
     private GoogleMap googleMap;
+    private Double [] coordenatesMetroStations = {6.1546144,-75.6214601,6.174159, -75.597090
+    ,6.185886, -75.585556,6.193230, -75.582327,6.212216, -75.578070};
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,7 +36,18 @@ public class FragmentRoutes extends Fragment {
         if (googleMap == null) {
             googleMap = ((MapFragment) getActivity().getFragmentManager().findFragmentById(
                     R.id.map)).getMap();
-
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+            googleMap.getUiSettings().setZoomGesturesEnabled(true);
+            googleMap.getUiSettings().setCompassEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            googleMap.getUiSettings().setRotateGesturesEnabled(true);
+            PolylineOptions polylineOptions = new PolylineOptions();
+            polylineOptions.color(Color.parseColor("#00FF00"));
+            for(int i = 0; i < coordenatesMetroStations.length; i=i+2){
+                    polylineOptions.add(new LatLng(coordenatesMetroStations[i], coordenatesMetroStations[i+1]));
+            }
+            googleMap.addPolyline(polylineOptions);
             // check if map is created successfully or not
             if (googleMap == null) {
                 Toast.makeText(getActivity().getApplicationContext(),
