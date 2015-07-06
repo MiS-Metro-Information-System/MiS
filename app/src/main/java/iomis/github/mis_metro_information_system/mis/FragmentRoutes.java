ba968a3952b6cf0fa8d6a1486843e995fb8a21f1
felipe.gutierrez <felipegi91@gmail.com>
2015-07-06 11:59:35 -0500

    android maps libraries , fragment and

--- app/src/main/java/iomis/github/mis_metro_information_system/mis/FragmentRoutes.java
+++ app/src/main/java/iomis/github/mis_metro_information_system/mis/FragmentRoutes.java
@@ -3,17 +3,47 @@ package iomis.github.mis_metro_information_system.mis;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 /**
  * Created by felipe.gutierrez on 06/07/2015.
  */
 public class FragmentRoutes extends Fragment {
+    private GoogleMap googleMap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        try{
+            initializeMap();
+        }catch (Exception e){
+            Log.e("Exception", e.toString());
+        }
         return inflater.inflate(R.layout.fragment_routes, container, false);
+    }
+    private void initializeMap(){
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getActivity().getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        initializeMap();
     }
 }
